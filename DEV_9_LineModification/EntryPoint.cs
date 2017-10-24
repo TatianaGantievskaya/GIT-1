2295001a338ffd4ebbc9cb252889ff1e0a7ff65b
TatianaGantievskaya <ta.gantievskaya@gmail.com>
2017-10-24 15:13:57 +0300

    add DEV_9

---
+++ DEV_9_LineModification/EntryPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LineModification
+{
+  /// <summary>
+  /// This is the entry point of the program.
+  /// </summary>
+  class EntryPoint
+  {
+    static void Main(string[] args)
+    {
+      string firstString = "he11oEvery1";
+      string secondString = "g00dBy8";
+      try
+      {
+        RandomLineModification changingString  = new RandomLineModification(firstString, secondString);
+        string modificatedString = changingString.ModificateString();
+        Console.WriteLine("Changing string: " + firstString + '\n' + "Replacing string: " + secondString);
+        Console.WriteLine("\nResulting string: " + modificatedString);
+      }
+      catch (IncorrectStringException er)
+      {
+        Console.WriteLine(er.Message);
+      }
+      Console.ReadKey();
+    }
+  }
+}
